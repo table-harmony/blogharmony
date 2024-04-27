cbebe3a4baafb7d2a211b714b19ad828099d5809
liron1111 <lironkaner2007@gmail.com>
2024-04-27 11:15:38 +0300

    Create LICENSE.md

--- velite.config.ts
+++ velite.config.ts
@@ -53,7 +53,7 @@ export default defineConfig({
   mdx: {
     rehypePlugins: [
       rehypeSlug,
-      [rehypePrettyCode, { theme: "github-dark" }],
+      [rehypePrettyCode, { theme: "one-dark-pro" }],
       [
         rehypeAutolinkHeadings,
         {
