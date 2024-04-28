cfc3c0ac6e74a9750714373b8c88efca5e110a85
liron1111 <lironkaner2007@gmail.com>
2024-04-28 15:36:13 +0300

    removing velite

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,15 +5,6 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export function formatDate(input: string | number): string {
-  const date = new Date(input);
-  return date.toLocaleDateString("en-US", {
-    month: "long",
-    day: "numeric",
-    year: "numeric",
-  });
-}
-
 export enum Theme {
   DARK = "dark",
   LIGHT = "light",
